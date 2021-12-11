241a416936474c130c891f6fb6b946f102dad568
Seth Thomas <sethpthomas91@gmail.com>
2021-12-11 18:29:08 -0500

    started user login and authentication

---
+++ backend/backend/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('token-auth/', obtain_jwt_token)
+]
