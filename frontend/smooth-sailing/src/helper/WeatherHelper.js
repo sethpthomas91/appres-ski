c18956f94857bafac2c5b1637c5ed71847d76959
Seth Thomas <sethpthomas91@gmail.com>
2021-12-17 14:43:40 -0500

    Hourly deconstructor helper function done for API call

--- frontend/smooth-sailing/src/helper/WeatherHelper.js
+++ frontend/smooth-sailing/src/helper/WeatherHelper.js
@@ -2393,9 +2393,34 @@ const forecast = {
   }
 }
 
-const hourlyDeconstructor = (forecast) => {
+// takes in an hourly api call and a date obj formatted in YYYY-MM-DD and returns a list of hourly weather objects
+const hourlyDeconstructor = (forecast, dateObj) => {
   const hourlyForecastObj = forecast.properties.periods
-
-  console.log(hourlyForecastObj)
+  // get the date from the dateObj
+  const date = dateObj.split('-')[2]
+  const hourArr = hourlyForecastObj.map( (hour) => {
+    // generate a new date time for each hour to compare against the hour
+    const hourDateObj = new Date(hour.startTime)
+    if (hourDateObj.getDate() == date) {
+      return obj = {
+        startHour : hour.startTime,
+        windSpeed : hour.windSpeed,
+        windDirection : hour.windDirection,
+        temperature : hour.temperature
+      }
+    } 
+  })
+  // filters all that are null or undefined
+  const filteredHourArr = hourArr.filter((obj) => {
+    return obj
+  } )
+  console.log(filteredHourArr)
+  return filteredHourArr
 }
+
+const helperFunctions = {
+  hourlyDeconstructor
+}
+
+export default helperFunctions 
 
